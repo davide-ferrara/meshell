6953429b6221e8820ab3feb18728403208f131a6
Davide Ferrara <davide98ferrara@gmail.com>
2025-11-05 00:13:04 +0100

    install fix

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ dest="/bin/meshell"
 
 sudo ln -sf "$origin" "$dest"
 cd scripts/
-make
-make install
+mv bin/* /bin
 
 echo "meshell é stato installato!"
 echo "Esegui meshell --h per visualizzare i comandi."
